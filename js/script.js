8f6b63852c0bfdad515177a24ff6918e427c325b
j-ohene <jesicaohene1@gmail.com>
2023-11-12 03:16:38 -0500

    added video

--- js/script.js
+++ js/script.js
@@ -33,6 +33,29 @@
   min = 0, 
   max = imageCon.offsetWidth;
 
+
+  let tl = gsap.timeline({
+    // yes, we can add it to an entire timeline!
+    scrollTrigger: {
+      trigger: ".player-container",
+      start: "top top", // when the top of the trigger hits the top of the viewport
+      endTrigger:"#end-vid",
+      end: "+=500", // end after scrolling 500px beyond the start
+      scrub: true, 
+      snap: {
+        snapTo: "labels", // snap to the closest label in the timeline
+        duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+        delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+        ease: "power1.inOut", // the ease of the snap animation ("power3" by default)
+      },
+    },
+  });
+  
+  // add animations and labels to the timeline
+  tl.addLabel("start")
+    .from(".player-container", {  })
+    .to("#end-vid", { })
+    .addLabel("end");
 //functions
 function earbudsLoaded() {
   hotSpots.forEach(hotspot => {
